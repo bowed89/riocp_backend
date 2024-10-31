78e7c5a1e2679c281966cff9df554efd67b042a4
bowed89 <jesus.lopez.averanga@gmail.com>
2024-10-31 14:47:36 -0400

    commit jueves 2

---
+++ app/Http/Requests/Revisor/SeguimientoRevisorRequests.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Http\Requests\Revisor;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class AsignarRevisoraRequest extends FormRequest
+{
+    public function rules()
+    {
+        return [
+            'id_seguimiento' => 'required|integer',
+            'observacion' => 'required|string',
+            'solicitud_id' => 'required|integer',
+            'usuario_destino_id' => 'required|integer',
+            // Observaciones
+            'observaciones' => 'required|array',
+            'observaciones.*.cumple' => 'required|boolean',
+            'observaciones.*.observacion' => 'required|string',
+            'observaciones.*.tipo_observacion_id' => 'required|integer',
+        ];
+    }
+
+    public function authorize()
+    {
+        return true;
+    }
+}
---
+++ app/Http/Services/Revisor/SeguimientoRevisorService.php
@@ -0,0 +1,147 @@
+<?php
+
+namespace App\Http\Services\Revisor;
+
+use App\Models\Observacion;
+use App\Models\Seguimientos;
+use App\Models\Solicitud;
+use Carbon\Carbon;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\DB;
+
+class SeguimientoRevisorService
+{
+    public function obtenerSeguimientos()
+    {
+        $user = Auth::user();
+
+        if (!$user) {
+            return [
+                'status' => 403,
+                'data' => [
+                    'status' => false,
+                    'message' => 'Usuario no autorizado o sin rol asignado.'
+                ]
+            ];
+        }
+
+        $resultados = Seguimientos::select(
+            'so.nro_solicitud',
+            's.id AS id_seguimiento',
+            DB::raw('COALESCE(s.created_at::text, \'SIN DATOS\') AS fecha_recepcion'),
+            DB::raw('COALESCE(s.fecha_derivacion::text, \'SIN DATOS\') AS fecha_derivacion'),
+            DB::raw('COALESCE(s.observacion, \'SIN DATOS\') AS observacion'),
+            DB::raw('so.id as solicitud_id'),
+            DB::raw('u_origen.nombre AS nombre_origen'),
+            DB::raw('u_origen.apellido AS apellido_origen'),
+            DB::raw('u_destino.nombre AS nombre_destino'),
+            DB::raw('u_destino.apellido AS apellido_destino'),
+            DB::raw('COALESCE(so.nro_hoja_ruta, \'SIN DATOS\') AS nro_hoja_ruta'),
+            DB::raw('COALESCE(r_origen.rol, \'SIN DATOS\') AS rol_origen'),
+            DB::raw('COALESCE(r_destino.rol, \'SIN DATOS\') AS rol_destino'),
+            DB::raw('COALESCE(ed.tipo, \'SIN DATOS\') AS estado')
+        )
+            ->from('seguimientos AS s')
+            ->join('solicitudes AS so', 's.solicitud_id', '=', 'so.id')
+            ->join('estados_requisito AS er', 'er.id', '=', 'so.estado_requisito_id')
+            ->join('estados_derivado AS ed', 'ed.id', '=', 's.estado_derivado_id')
+            ->join('usuarios AS u_origen', 'u_origen.id', '=', 's.usuario_origen_id')
+            ->join('roles AS r_origen', 'r_origen.id', '=', 'u_origen.rol_id')
+            ->join('usuarios AS u_destino', 'u_destino.id', '=', 's.usuario_destino_id')
+            ->join('roles AS r_destino', 'r_destino.id', '=', 'u_destino.rol_id')
+            ->where('u_destino.rol_id', $user->rol_id)
+            ->where('u_destino.id', $user->id)
+            ->get();
+
+        if ($resultados->isEmpty()) {
+            return [
+                'status' => 200,
+                'data' => [
+                    'status' => false,
+                    'message' => 'No se encontraron seguimientos.'
+                ]
+            ];
+        }
+
+        return [
+            'status' => 200,
+            'data' => [
+                'status' => true,
+                'message' => 'Listado de seguimientos.',
+                'data' => $resultados,
+            ]
+        ];
+    }
+
+    public function asignarRevisora($data)
+    {
+        $user = Auth::user();
+        if (!$user) {
+            return [
+                'status' => 401,
+                'data' => [
+                    'status' => false,
+                    'message' => 'Usuario no autorizado o sin rol asignado.'
+                ]
+            ];
+        }
+
+        $solicitud = Solicitud::where('id', $data['solicitud_id'])->first();
+
+        if (!$solicitud) {
+            return [
+                'status' => 400,
+                'data' => [
+                    'status' => false,
+                    'message' => 'No existe una solicitud.'
+                ]
+            ];
+        }
+         // actualizar seguimiento de jefe de unidad a revisor
+         $seguimientoOrigen = Seguimientos::where('id', $data['id_seguimiento'])->first();
+
+        if (!$seguimientoOrigen) {
+            return [
+                'status' => 400,
+                'data' => [
+                    'status' => false,
+                    'message' => 'No existe un seguimiento origen.'
+                ]
+            ];
+        }
+
+        $seguimientoOrigen->estado_derivado_id = 2;
+        $seguimientoOrigen->observacion = $data['observacion'];
+        $seguimientoOrigen->fecha_derivacion = Carbon::now();
+        $seguimientoOrigen->save();
+
+        // Agregar seguimiento para la próxima unidad
+        $seguimiento = new Seguimientos();
+        $seguimiento->solicitud_id = $data['solicitud_id'];
+        $seguimiento->usuario_origen_id = $user->id;
+        $seguimiento->usuario_destino_id = $data['usuario_destino_id'];
+        $seguimiento->estado_derivado_id = 1;
+        $seguimiento->save();
+
+        // Agregar observaciones
+        foreach ($data['observaciones'] as $observacion) {
+            $newObservacion = new Observacion();
+            $newObservacion->cumple = $observacion['cumple'];
+            $newObservacion->observacion = $observacion['observacion'];
+            $newObservacion->tipo_observacion_id = $observacion['tipo_observacion_id'];
+            $newObservacion->solicitud_id = $solicitud->id;
+            $newObservacion->usuario_id = $user->id;
+            $newObservacion->rol_id = $user->rol_id;
+            $newObservacion->save();
+        }
+
+        return [
+            'status' => 200,
+            'data' => [
+                'status' => true,
+                'message' => 'Seguimiento registrado.',
+                'data' => $seguimiento
+            ]
+        ];
+    }
+}
